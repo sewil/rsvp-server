7494724a3b854b53e30dc06693ad20383858c8b3
sewil <1498414+sewil@users.noreply.github.com>
2025-04-11 18:09:32 +0200

    Copy patch.base

--- WvsBeta.PatchCreator/MakePatches.bat
+++ WvsBeta.PatchCreator/MakePatches.bat
@@ -1,4 +1,5 @@
 @echo off
+echo f | xcopy /f /y patch.base bin\Debug\patches\patch.base
 echo f | xcopy /f /y Patcher.exe bin\Debug\patches\NewPatcher.dat
 cd bin\Debug
 WvsBeta.PatchCreator.exe make-patches
