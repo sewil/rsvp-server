85a000ffa9fbd35d827e48748a4606a4f925e640
sewil <1498414+sewil@users.noreply.github.com>
2025-05-28 18:32:43 +0200

    Update SignDataFile.bat

--- SignDataFile.bat
+++ SignDataFile.bat
@@ -1,1 +1,1 @@
-BinSvr\WvsBeta.AdminKeygen.exe sign ..\rsvp-data\Data.wz DataSvr\server_private.key.xml %*
+BinSvr\WvsBeta.AdminKeygen.exe sign ..\rsvp-data\Data.wz DataSvr\server_private.key.xml ..\rsvp-client\Data.wz
